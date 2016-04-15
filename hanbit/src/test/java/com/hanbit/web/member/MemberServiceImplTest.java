f182d6dbad470fdc3b287aef2f5104b5cea2db30
pakjkwan <pakjkwan@gmail.com>
2016-04-15 14:23:38 +0900

    댓글 코딩

--- hanbit/src/test/java/com/hanbit/web/member/MemberServiceImplTest.java
+++ hanbit/src/test/java/com/hanbit/web/member/MemberServiceImplTest.java
@@ -1,95 +1,28 @@
-/**
- * 
- */
 package com.hanbit.web.member;
 
 import static org.junit.Assert.*;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hanbit.web.mapper.MemberMapper;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InOrder;
-import static org.mockito.Mockito.*;
-import org.mockito.Mock;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-/**
- * @author Administrator
- *
- */
-@RunWith(value = SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {MyServiceInjectingMocksTest.MemberServiceTestConfig.class, MyAppConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/*-context.xml")
 public class MemberServiceImplTest {
-	private MemberService memberService;
-	private MemberMapper memberMapper;
-	private MemberDTO member;
-
-    @Before
-    public void setupMock() {
-    	memberService = new MemberServiceImpl();
-    	member = mock(MemberDTO.class);
-    	memberMapper = mock(MemberMapper.class);
-    }
-    @Test
-    public void testMockCreation(){
-        assertNotNull(member);
-    }
-	@Test
-	public void testJoin() {
-		fail("Not yet implemented");
-	}
-
+	@Autowired MemberDTO member;
+	@Autowired SqlSession session;
 	@Test
 	public void testLogin() {
-		// fail("Not yet implemented");
-		
-	}
-
-	@Test
-	public void testUpdate() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemove() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDetail() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIsMember() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetList() {
-		fail("Not yet implemented");
+		MemberMapper mapper = session.getMapper(MemberMapper.class);
+		member.setId("hong");
+		member.setPassword("1");
+		member = mapper.login(member);
+		assertEquals("홍길동",member.getName());
 	}
 	
-	@Test
-	public void testCount() {
-		fail("Not yet implemented");
-	}
-	static class MemberServiceTestConfig{
-        /**
-         * @Primary indicates Spring to use this bean in priority to the one that is component-scanned
-         */
-        @Bean
-        @Primary
-        public MemberService getHelloService(){
-            return mock(MemberService.class);
-        }
-    }
 }
--- hanbit/src/test/java/com/hanbit/web/test/CalculatorTest.java
+++ hanbit/src/test/java/com/hanbit/web/test/CalculatorTest.java
@@ -1,14 +1,20 @@
 package com.hanbit.web.test;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/*-context.xml")
 public class CalculatorTest {
-	Calculator c = new Calculator();
+	@Autowired Calculator c;
 	@Test
 	public void testSum() {
-		assertEquals(7, c.sum(3, 7));
+		assertThat(c.sum(3, 7),is(10));
 	}
 
 }
