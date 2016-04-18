f9afd12a69143653611004bbc34df3e3626f6234
pakjkwan <pakjkwan@gmail.com>
2016-04-18 15:22:11 +0900

    과목별 교수 아이디 추가

--- hanbit/src/main/java/com/hanbit/web/admin/admin.sql
+++ hanbit/src/main/java/com/hanbit/web/admin/admin.sql
@@ -10,5 +10,11 @@ CREATE TABLE Admin(
 
 INSERT INTO Admin(id,name,password,addr,birth,role)
 VALUES ('admin','관리자','1','서울',800101,'admin');
+INSERT INTO Admin(id,name,password,addr,birth,role)
+VALUES ('prof_kim','김교수','1','인천',750101,'prof');
+INSERT INTO Admin(id,name,password,addr,birth,role)
+VALUES ('prof_lee','이교수','1','수원',851201,'prof');
+INSERT INTO Admin(id,name,password,addr,birth,role)
+VALUES ('prof_park','박교수','1','의정부',830801,'prof');
 
 SELECT * FROM Admin WHERE id = 'admin' and password = '1';
--- hanbit/src/main/java/com/hanbit/web/subject/subject.sql
+++ hanbit/src/main/java/com/hanbit/web/subject/subject.sql
@@ -2,16 +2,25 @@ DROP TABLE Subject;
 
 CREATE TABLE Subject(
 	subj_seq INT(4) PRIMARY KEY AUTO_INCREMENT,
-	subj_name VARCHAR(30)
+	subj_name VARCHAR(30),
+	prof_id VARCHAR(30)
+	CONSTRAINT subj_admin_fk FOREIGN KEY prof_id
+	REFERENCES Admin(id)
 );
-INSERT INTO Subject(subj_name) VALUES('java');
-INSERT INTO Subject(subj_name) VALUES('jsp');
-INSERT INTO Subject(subj_name) VALUES('spring');
-INSERT INTO Subject(subj_name) VALUES('sql');
-INSERT INTO Subject(subj_name) VALUES('nodejs');
-INSERT INTO Subject(subj_name) VALUES('python');
-INSERT INTO Subject(subj_name) VALUES('android');
+INSERT INTO Subject(subj_name,prof_id) VALUES('java','prof_kim');
+INSERT INTO Subject(subj_name,prof_id) VALUES('jsp','prof_kim');
+INSERT INTO Subject(subj_name,prof_id) VALUES('spring','prof_lee');
+INSERT INTO Subject(subj_name,prof_id) VALUES('sql','prof_lee');
+INSERT INTO Subject(subj_name,prof_id) VALUES('nodejs','prof_park');
+INSERT INTO Subject(subj_name,prof_id) VALUES('python','prof_park');
+INSERT INTO Subject(subj_name,prof_id) VALUES('android','prof_park');
 
+UPDATE Subject SET prof_id = 'prof_kim'
+WHERE subj_name = 'java' OR subj_name = 'jsp';
+UPDATE Subject SET prof_id = 'prof_lee'
+WHERE subj_name = 'spring' OR subj_name = 'sql';
+UPDATE Subject SET prof_id = 'prof_park'
+WHERE subj_name = 'nodejs' OR subj_name = 'python' OR subj_name = 'android';
 
 SELECT * FROM Subject;
 
@@ -21,3 +30,8 @@ SELECT * FROM Subject;
 
 DROP TABLE Subject CASCADE ;
 
+ALTER TABLE Subject ADD COLUMN prof_id VARCHAR(30);
+
+ALTER TABLE Subject
+ADD FOREIGN KEY (prof_id)
+REFERENCES Admin(id);
