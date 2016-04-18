ddb5d364555b411f1a3666fb1b40b4f57ea9e05a
pakjkwan <pakjkwan@gmail.com>
2016-04-18 11:51:39 +0900

    ReplyDTO 를 커밋

---
+++ hanbit/src/main/java/com/hanbit/web/board/ReplyDTO.java
@@ -0,0 +1,29 @@
+package com.hanbit.web.board;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReplyDTO {
+	private int replySeq;
+	private int articleId;
+	private String reply;
+	public int getReplySeq() {
+		return replySeq;
+	}
+	public int getArticleId() {
+		return articleId;
+	}
+	public String getReply() {
+		return reply;
+	}
+	public void setReplySeq(int replySeq) {
+		this.replySeq = replySeq;
+	}
+	public void setArticleId(int articleId) {
+		this.articleId = articleId;
+	}
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+	
+}
